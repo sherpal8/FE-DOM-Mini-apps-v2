930c3af6b0268afee63e395a9ebdfa5e8fb1808c
sherpal <sherpalkorotana@icloud.com>
2019-10-01 07:59:20 +0100

    append child to list

---
+++ index.js
@@ -0,0 +1,23 @@
+const list = document.querySelector("ul");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const listItems = document.querySelectorAll("li");
+
+const addTodo = event => {
+  event.preventDefault();
+  const newTodo = input.value;
+  const newElement = document.createElement("li");
+  newElement.innerText = newTodo;
+  list.appendChild(newElement);
+  input.value = "";
+};
+
+form.addEventListener("submit", addTodo);
+
+const onClick = event => {
+  item.classList.add("crossOut");
+};
+
+listItems.forEach(item => {
+  item.addEventListener("click", onClick);
+});
